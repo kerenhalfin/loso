ddbc7c78d317fd061f38dbf9cb952e4eb229a41a
kerenhalfin <47307864+kerenhalfin@users.noreply.github.com>
2020-11-06 15:11:07 -0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,127 @@
+$(window).on("load", function () {
+    $("body").css("overflow-y", "scroll");
+    $(".spinner-wrapper").fadeOut("slow");
+});
+
+// Show and hide menu
+$(document).ready(function () {
+    'use strict';
+
+    $(window).scroll(function () {
+        'use strict';
+        let marginTop, opacity, backgroundColor;
+
+        if ($(window).scrollTop() < 80) {
+            marginTop = '-100px';
+            opacity = '0';
+            backgroundColor = 'rgb(0 0 0 / 0%)';
+
+        } else {
+            marginTop = '0px';
+            opacity = '1';
+            backgroundColor = 'rgb(0 0 0 / 80%)';
+        }
+
+        $('.navbar').css({
+            'margin-top': marginTop,
+            'opacity': opacity
+        });
+
+        $('.navbar-default').css({
+            'background-color': backgroundColor
+        });
+    });
+});
+
+// active menu item on click
+$(document).ready(function () {
+    'use strict';
+
+    $('.navbar-nav > li > a').click(function () {
+        'use strict';
+
+        // remove the active class from the previous anchor
+        $('.navbar-nav > li').removeClass('active');
+        // add active class to the current clicked anchor
+        $(this).parent().addClass('active');
+    });
+});
+
+// highlight relevant section on the menu when the user scrolls through the section
+$(document).ready(function () {
+    'use strict';
+
+    $(window).scroll(function () {
+        'use strict';
+
+        $('section').each(function () {
+            'use strict';
+
+            let sectionId = $(this).attr('id');
+            let outerHeight = $(this).outerHeight();
+            let greaterTop = $(this).offset().top - 70;
+
+            // when the user scrolls 70 pixels before the section
+            // find out the relevant section - add active css class to it, remove it from the rest of the sections
+            if (($(window).scrollTop() > greaterTop) &&
+                ($(window).scrollTop() < (greaterTop + outerHeight))) {
+                $('.navbar-nav li a[href=\'#' + sectionId + '\']').parent().addClass('active');
+            } else {
+                $('.navbar-nav li a[href=\'#' + sectionId + '\']').parent().removeClass('active');
+            }
+        });
+    });
+});
+
+// adjust the top+bottom padding of the header container so it would fill the screen nicely
+$(document).ready(function () {
+    'use strict';
+
+    setInterval(function () {
+        'use strict';
+
+        let windowHeight = $(window).height();
+        let containerHeight = $('.header-container').height();
+        let padTop = windowHeight - containerHeight;
+
+        $('.header-container').css({
+            'padding-top': Math.round(padTop / 2) + 'px',
+            'padding-bottom': Math.round(padTop / 2) + 'px'
+        });
+
+    }, 10);
+});
+
+// add a bxslider in the screens section
+$(document).ready(function () {
+    'use strict';
+
+    $('.bxslider').bxSlider({
+        slideWidth: 292.5,
+        auto: true,
+        minSlides: 1,
+        maxSlides: 3,
+        slideMargin: 50
+    });
+});
+
+// add counter up animation
+$(document).ready(function () {
+
+    let viewportWidth = $(window).width();
+
+    // add counter animation only excluding small devices
+    if (viewportWidth > 767) {
+        $('.counter-num').counterUp({
+            delay: 10,
+            time: 1500
+        });
+    }
+});
+
+// add animation
+$(document).ready(function () {
+    'use strict';
+
+    new WOW().init();
+});
